d3f0afff1c147c4c18c302c3deb94c712b2f4e58
nopalll <tssytari@gmail.com>
2026-02-04 06:16:03 +0700

    edit login

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -100,20 +100,7 @@ const LoginForm = ({ onSubmit, loading = false }) => {
           }
         />
 
-        {/* Remember Me */}
         <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <input
-              id="remember-me"
-              name="remember-me"
-              type="checkbox"
-              className="h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 rounded"
-            />
-            <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-              Ingat saya
-            </label>
-          </div>
-
           <div className="text-sm">
             <Link 
               to="/forgot-password" 
